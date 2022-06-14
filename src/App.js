f343fd40454c3a0fd1ab117fce5f69e94ba10240
elias <99310710+bo-codes@users.noreply.github.com>
2022-06-14 14:18:39 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import Home from "./components/Home";
+import Stocks from "./components/Stocks";
+import Movies from "./components/Movies";
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="main">
+      <h1>App Component</h1>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/stocks">
+          <Stocks />
+        </Route>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route>
+          <h1>Page Not Found</h1>
+        </Route>
+      </Switch>
+      {/* <Home />
+      <Stocks />
+      <Movies /> */}
+    </div>
+  );
+}
+
+export default App;
